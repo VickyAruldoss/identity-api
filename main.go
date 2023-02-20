eb49e84b377354fb5c32c4f166f9aa3f37dfda94
Vicky Aruldoss <jamvicky3094@gmail.com>
2023-02-20 22:55:32 +0530

    Imported port from the config file

--- main.go
+++ main.go
@@ -3,14 +3,20 @@ package main
 import (
 	"fmt"
 
+	"github.com/VickyAruldoss/identity-api/configuration"
 	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
 	app := fiber.New()
+	config, err := configuration.NewConfigLoader().Load("config.json", "")
+	if err != nil {
+		fmt.Println("error while loading config", err)
+	}
 	fmt.Println("app started in the port 3000")
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("bull shit")
 	})
-	app.Listen(":3000")
+	addr := ":" + config.Port
+	app.Listen(addr)
 }
